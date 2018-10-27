ded81bb0cb06dda8d1495e46cab15708f8d8c6dc
jponly <jpangeli514@gmail.com>
2018-10-27 12:50:54 -0700

    versionOct27

--- routes/api/games.js
+++ routes/api/games.js
@@ -6,11 +6,16 @@ router.route("/")
   .get(gamesController.findAll)
   .post(gamesController.create);
 
-// Matches with "/api/games/:id"
+ // Matches with "/api/games/live 
+ router.route("/live")
+ .get(gamesController.live);
+
+ // Matches with "/api/games/:id"
 router
   .route("/:id")
   .get(gamesController.findById)
   .put(gamesController.update)
   .delete(gamesController.remove);
+
 
 module.exports = router;
