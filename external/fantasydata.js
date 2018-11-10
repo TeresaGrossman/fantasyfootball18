b9b323efb87d4b5bc47b728a73063d26ae709907
Shirin <shirin.boroujeni@gmail.com>
2018-11-10 11:56:58 -0800

    .env

--- external/fantasydata.js
+++ external/fantasydata.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 var request = require("request");
 
 function doFantasyAPI(url, cb) {
@@ -7,7 +8,7 @@ function doFantasyAPI(url, cb) {
         url: url,
         method: 'GET',
         headers: {
-            'Ocp-Apim-Subscription-Key': '8e2069e6dccf47a28fd0f70ba78ffaeb'
+            'Ocp-Apim-Subscription-Key': process.env.key
         }
     };
 
